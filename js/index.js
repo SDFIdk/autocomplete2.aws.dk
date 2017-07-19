656a72b580a6f89a4786009c5810c587e989d5d1
finnjordal <finn@jordal.dk>
2017-07-19 15:04:11 +0200

    Guide

---
+++ js/index.js
@@ -0,0 +1,49 @@
+"use strict"
+
+ dawaAutocomplete.dawaAutocomplete(document.getElementById("adresse"), {
+    select: function(selected) {
+    document.getElementById("valgtadresse").innerHTML= selected.tekst;
+      console.log('Selected address:');
+      console.dir(selected);
+    }
+  });
+
+  dawaAutocomplete.dawaAutocomplete(document.getElementById("kbhadresse"), {
+    params: {
+      kommunekode: 101
+    },
+    select: function(selected) {
+    	document.getElementById("valgtkbhadresse").innerHTML= selected.tekst;
+      console.log('Selected address:');
+      console.dir(selected);
+    }
+  }); 
+
+  dawaAutocomplete.dawaAutocomplete(document.getElementById("sbadresse"), {
+    params: {
+      postnr: 5700
+    },
+    select: function(selected) {
+    	document.getElementById("valgtsbadresse").innerHTML= selected.tekst;
+      console.log('Selected address:');
+      console.dir(selected);
+    }
+  });
+
+  dawaAutocomplete.dawaAutocomplete(document.getElementById("adgangsadresse"), {
+    adgangsadresserOnly: true,
+    select: function(selected) {
+    	document.getElementById("valgtadgangsadresse").innerHTML= selected.tekst;
+      console.log('Selected address:');
+      console.dir(selected);
+    }
+  });
+
+  dawaAutocomplete.dawaAutocomplete(document.getElementById("mladresse"), {
+    multiline: true,
+    select: function(selected) {
+    	document.getElementById("valgtmladresse").innerHTML= selected.tekst;
+      console.log('Selected address:');
+      console.dir(selected);
+    }
+  });
